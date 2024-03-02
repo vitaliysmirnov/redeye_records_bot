6e81c9f0c64fbbfe91dae743108e496a51f2e514
vitaliysmirnov <vitaliysmirnov2323@gmail.com>
2024-03-02 13:02:25 +0700

    Unused code removed

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 #!/usr/bin/env python
 #
 # -*- coding: utf-8 -*-
-
-import requests
-from app.config import *
-
-user_chat_id = 230217326
-selection = "bass_music"
-
-requests.put(f"{API_HOST}/subscribe?user_chat_id={user_chat_id}&selection={selection}")
