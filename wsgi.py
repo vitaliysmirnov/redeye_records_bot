67fd83fd2f95dc4748079abe861d031e73524ef2
vitaliysmirnov <vitaliysmirnov2323@gmail.com>
2024-03-07 00:05:51 +0700

    Removed logging from wsgi.pt as soon as it is not working

--- wsgi.py
+++ wsgi.py
@@ -2,11 +2,7 @@
 #
 # -*- coding: utf-8 -*-
 
-import logging
-
 from app.bot import app
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, filename="redeye_records_bot.log", filemode="w", format="%(asctime)s %(levelname)s %(message)s")
-
     app.run()
