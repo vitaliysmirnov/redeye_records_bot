30de295c6954673741a06b9d0539a4dfaff7e01a
vitaliysmirnov <vitaliysmirnov2323@gmail.com>
2024-03-08 13:23:45 +0700

    help text fix

--- app/api.py
+++ app/api.py
@@ -581,10 +581,10 @@ class Help(Resource):
     @require_api_key
     def get(self):
         """A list of available actions"""
-        help_text = "*redeyerecords.co.uk* - dance music specialists since 1992\n\n\n" \
+        help_text = "🌐 *redeyerecords.co.uk* — dance music specialists since 1992\n\n" \
                     "*/selections* to choose selections\n\n" \
                     "*/my_subscriptions* to get your subscriptions\n\n" \
-                    "*/unsubscribe* to unsubscribe\n\n"
+                    "*/unsubscribe* to unsubscribe"
         try:
             return help_text, 200
 
