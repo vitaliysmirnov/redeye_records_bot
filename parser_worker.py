af363c15749dcdfe9af17d6243534163f40e34fb
vitaliysmirnov <vitaliysmirnov2323@gmail.com>
2024-12-18 14:20:39 +0700

    added time rotated logging

--- parser_worker.py
+++ parser_worker.py
@@ -2,6 +2,7 @@
 
 import os
 import logging
+from logging.handlers import TimedRotatingFileHandler
 
 from config import basedir
 from app.parser import Parser
@@ -15,12 +16,12 @@ def main(p):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.DEBUG,
-        filename=os.path.join(basedir, "parser_worker.log"),
-        filemode="a",
-        format="%(asctime)s %(levelname)s %(message)s"
-    )
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    handler = TimedRotatingFileHandler(os.path.join(basedir, "parser_worker.log"), when="midnight", backupCount=10)
+    formatter = logging.Formatter(fmt="%(asctime)s %(levelname)s %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
     parser = Parser()
     main(parser)
